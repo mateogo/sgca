d9da085011c01ec91d952e0207b5717ae2d24311
Ernest Venencio <zayrus@gmail.com>
2014-11-14 16:41:13 -0300

    commentario ssl

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongodb.connect(config);
 
 var app = express();
 config.routesBootstrap(app,express);
-
+//se agrego https conexion ssl
 var server = https.createServer(options, app);
 server.listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
