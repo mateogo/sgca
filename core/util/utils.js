9aeb20037ea5e20b1356c33140e258eacfe81496
Ernest Venencio <zayrus@gmail.com>
2015-01-21 14:09:20 -0300

    REV: MICA2015

--- core/util/utils.js
+++ core/util/utils.js
@@ -110,7 +110,7 @@ var rutas = {
     'gestion:comprobantes:list'    :'/gestion/#comprobantes',
     'sisplan:acciones:list'        :'/sisplan/#acciones',
     'studio:producciones:list'     :'/studio',
-    'mica:rondas'    			   :'/mica2015/#rondas'
+    'mica:rondas'    			   :'/mica2015/#rondas/nuevo'
 };
 
 
